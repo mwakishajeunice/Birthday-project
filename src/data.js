9e11f2b5029cc6c1cd9c6106a35378d457ccb17e
mwakishajeunice <mwakishajeunice2017@gmail.com>
2021-04-24 09:03:59 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const Data = [
+    {
+        id: 1,
+        name: "Mwakisha Jeunice",
+        age: 23,
+        image: "src\images\fav.jpg",
+    },
+    {
+        id: 2,
+        name: "Christopher Otieno",
+        age: 23,
+        image: "https://z-p3-scontent.fmba2-1.fna.fbcdn.net/v/t1.6435-9/157466231_1078299879332845_5345407615979022098_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=RquRqCOAqN4AX8DkiOi&_nc_ht=z-p3-scontent.fmba2-1.fna&oh=ddd93f0d1abf1d89936f682794323393&oe=60A75278",
+    },
+    {
+        id: 3,
+        name: "Xavier Oduor",
+        age: 21,
+        image: "src\images\frank.jpg",
+    },
+    {
+        id: 4,
+        name: "Angelinah Kirigha",
+        age: 23,
+        image: "src\images\choice.jpg",
+    },
+
+];
+
+export default Data;
